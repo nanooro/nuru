5025d50157a9dcece4a8750b31ce30a6e144098d
Akash <notnotsky26@gmail.com>
2025-07-12 10:24:45 +0000

    fix: remove duplicate import in store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,17 +1,4 @@
-import { create } from "zustand";
 
-type Article = {
-  id: number;
-  Heading: string;
-  subHeading: string;
-  imgUrl: string;
-  created_at: string;
-};
-
-type ArticleStore = {
-  articles: Article[];
-  fetchArticles: () => Promise<void>;
-};
 
 import { create } from 'zustand';
 import { articles as mockArticles } from './mockData';
